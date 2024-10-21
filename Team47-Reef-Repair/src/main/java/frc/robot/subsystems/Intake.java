f9fd8da2cca06403bd2219778562ae7f87c555a1
ColinH0 <119466406+ColinH0@users.noreply.github.com>
2024-10-21 17:31:46 -0400

    created intake.java (kathy)

---
+++ Team47-Reef-Repair/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+    private placeholderMotor1 = new placeholderMotor();
+
+    public Intake(){
+        // stuff something or other
+    }
+    
+    public void in(){
+        // rotate placeholderMotor inward to intake 
+        // help idk what motor we are using
+    }
+
+    public void out(){
+        // rotate placeholderMotor outward
+    }
+}
