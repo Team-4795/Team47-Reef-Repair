0d1e85b2c2b4793d72d870c0ce43b963036dfd12
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2024-11-19 16:42:48 -0500

    changed the motor for drive

--- Team47-Reef-Repair/src/main/java/frc/robot/subsystems/Drive.java
+++ Team47-Reef-Repair/src/main/java/frc/robot/subsystems/Drive.java
@@ -1,12 +1,12 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
-    private PWMVictorSPX leftMotor = new PWMVictorSPX(0);
-    private PWMVictorSPX rightMotor = new PWMVictorSPX(0);
+    private PWMTalonSRX leftMotor = new PWMTalonSRX(0);
+    private PWMTalonSRX rightMotor = new PWMTalonSRX(0);
     private DifferentialDrive diffDrive = new DifferentialDrive(leftMotor, rightMotor);
     public Drive (){
         
